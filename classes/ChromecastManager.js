a930d9308ac91d57a9860363d7a711c2060fa6ff
moransantiago <santimoran.1@hotmail.com>
2020-09-22 21:33:17 -0300

    Commands added

--- classes/ChromecastManager.js
+++ classes/ChromecastManager.js
@@ -1,20 +1,14 @@
 const ChromecastAPI = require('chromecast-api')
 
 class ChormecastManager {
-	constructor() {
-		this.devices = []
+	constructor(mediaURL) {
 		this.client = new ChromecastAPI()
+		this.devices = []
+		this.currentDevice = undefined
+		this.mediaURL = mediaURL
 
 		this.client.on('device', async device => {
 			this.addDevice(device)
-			try {
-				const device = await this.askForDevice(this.devices)
-				device.play(mediaURL, function (err) {
-					if (!err) console.log('Playing in your chromecast')
-				})
-			} catch (err) {
-				console.log(err)
-			}
 		})
 	}
 
@@ -22,28 +16,78 @@ class ChormecastManager {
 		this.devices.push({ name: device.friendlyName, device })
 	}
 
-	async askForDevice(devices) {
+	async askForDevice() {
 		return new Promise((res, rej) => {
 			console.log(
 				`Select a device to cast: (input the index 0 - ${
-					devices.length - 1
+					this.devices.length - 1
 				})`
 			)
 			console.log('Input "cancel" to cancel the action')
-			console.table(devices)
+			console.table(this.devices)
 
 			process.stdin.once('data', data => {
-				const choice = data.toString().trim().toUpperCase()
+				const choice = data.toString().trim().toLowerCase()
 
-				if (choice === 'CANCEL') {
+				if (choice === 'cancel') {
 					rej('Action canceled')
-				} else if (data < 0 || data > devices.length - 1) {
+				} else if (data < 0 || data > this.devices.length - 1) {
 					rej('Device not found')
 				} else {
-					res(devices[choice].device)
+					res(this.devices[choice].device)
 				}
 			})
 		})
+	}
+
+	async play(mediaURL, cb) {
+		try {
+			const device = await this.askForDevice(this.devices)
+			device.play(mediaURL, err => {
+				if (!err) console.log('Playing in your chromecast')
+				setTimeout(cb, 3000)
+			})
+			this.currentDevice = device
+		} catch (err) {
+			console.log(err)
+			cb()
+		}
+	}
+
+	pause(cb) {
+		this.currentDevice.pause(err => {
+			if (err) {
+				console.log(err)
+			}
+
+			cb()
+		})
+	}
+	
+	resume(cb) {
+		this.currentDevice.resume(err => {
+			if (err) {
+				console.log(err)
+			}
+	
+			cb()
+		})
+	}
+
+	stop(cb) {
+		this.currentDevice.stop(err => {
+			if (err) {
+				console.log(err)
+			}
+	
+			cb()
+		})
+	}
+
+	getDevices(cb) {
+		cb()
+
+		return this.devices
 	}
 }
 
--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 'use strict'
 
-const mediaURL = process.env.MEDIA_URL
-
-if (!mediaURL) {
-    throw new Error('You have to input a mediaURL!');
-}
-
 const ChormecastManager = require('./classes/ChromecastManager')
 
 const myChromecastClient = new ChormecastManager()
+
+const listenToUser = () => {
+	process.stdin.once('data', data => {
+		const input = data.toString().split(' ')
+		const command = input[0].trim().toLowerCase()
+        const args = input[1] && input[1].trim()
+
+		switch (command) {
+			case 'play':
+                if (!args) {
+                    console.log('Wrong usage, example: play https://www.youtube.com/watch?v=cU4315WzMyM');
+                    listenToUser()
+                } else {
+                    myChromecastClient.play(args, listenToUser)
+                }
+				break
+			case 'pause':
+				myChromecastClient.pause(listenToUser)
+				break
+			case 'resume':
+				myChromecastClient.resume(listenToUser)
+				break
+			case 'stop':
+				myChromecastClient.stop(listenToUser)
+				break
+			case 'devices':
+                const devices = myChromecastClient.getDevices(listenToUser)
+                console.table(devices);
+				break
+			case 'clear':
+                console.clear();
+                listenToUser()
+				break
+			default:
+				console.log('The input option is not valid')
+                listenToUser()
+				break
+		}
+	})
+}
+
+listenToUser()
